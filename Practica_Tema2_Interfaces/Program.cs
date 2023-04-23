9b0ac6cc3ae462e9db45be24c4d3eadaf12eaf6f
Ruxandra Boghean <ruxandraboghean@gmail.com>
2023-04-23 23:21:04 +0300

    add second homework

---
+++ Practica_Tema2_Interfaces/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Practica_Tema2_Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In Main (Console.App project), create a SmallApartment, a person to live in it, and to show the data of the person (Use interfaces if you want to add
+            //complexity)
+        }
+    }
+
+    //Interface
+    interface IPersonData
+    {
+        void showData(double _area, string _color);
+    }
+
+    interface IHouseData
+    {
+        void 
+    }
+
+    //Person implements IHome interface
+}
